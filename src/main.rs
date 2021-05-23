b5b115bd23beb3220ab85003fdc1cb456086e014
PartlyAwesome <5777950+PartlyAwesome@users.noreply.github.com>
2021-05-24 00:43:02 +0100

    Set up inject and extract routes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-extern crate exitcode;
-
 use std::{env, process};
 
 fn main() {
 	let args: Vec<_> = env::args().collect();
 
-	if args.len() <= 2 {
-		println!("Usage:");
-		println!("rsgct -e file.gct [-w width] [-h height] [-s start_byte_hex]");
-		println!("rsgct -i file.png -t file.gct [-s start_byte_hex] [-wd (write dimensions)]");
+	if args.len() <= 3 {
+		usage();
 		process::exit(exitcode::USAGE);
 	}
 	
+	let mode_request = &args[1];
+	match mode_request.as_str() {
+		"-e" => extract(),
+		"-i" => inject(),
+		_ => {
+			println!("Invalid operating mode.\n");
+			usage();
+			process::exit(exitcode::USAGE);
+		},
+	}
+}
+
+fn usage() {
+	println!("Usage:");
+	println!("rsgct -e file.gct [-w width] [-h height] [-s start_byte_hex]");
+	println!("rsgct -i file.png -t file.gct [-s start_byte_hex] [-wd (write dimensions)]");
+}
+
+fn extract() {
+
+}
+
+fn inject() {
+
 }
